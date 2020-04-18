a32f0652986f391e29722e491fe6f6046ef1bfd6
Colemayne <coleman@beilers.com>
2020-04-18 13:12:18 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <div class="background" />
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+  }),
+};
+</script>
